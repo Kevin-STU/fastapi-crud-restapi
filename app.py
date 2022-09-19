10ee4ce0e37d13188aab4f949de095b9ad7c604a
Kevin Prada <82003549+Kevin-STU@users.noreply.github.com>
2022-09-18 21:24:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,59 @@
-from cgitb import text
-from datetime import datetime
-from lib2to3.pytree import Base
-import string
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Text, Optional
+from typing import Optional, Text
 from datetime import datetime
 from uuid import uuid4 as uuid
+import uvicorn
 
 app = FastAPI()
 
 posts = []
 
-# Posts Model
-# esquema de cómo van a lucir las publicacinones
+# Post model
 class Post(BaseModel):
     id: Optional[str]
     title: str
     author: str
     content: Text
-    created_at: datetime = datetime.now()
-    published_at: Optional[datetime]
-    published: bool = False
-#creando rutas
-@app.get('/') #Get método HTTP y '/' nombre de la ruta 
+    created_at: datetime =  datetime.now()
+    published_at: Optional[datetime] 
+    published: Optional[bool] = False
+
+@app.get('/')
 def read_root():
-    return {"Welcome": "Bienvenido a mi REST API"}
+    return {"welcome": "Welcome to my API"}
 
 @app.get('/posts')
 def get_posts():
     return posts
 
-@app.post('/posts') 
-#creando una función que cuando visite /posts a través del método post
+@app.post('/posts')
 def save_post(post: Post):
-    post.id = str(uuid()) #genera un string aleatorio y se la asigna a la propiedad id de la publicación 
-    posts.append(post.dict()) #después lo convierte en diccionario y luego lo añade a arreglo
+    post.id = str(uuid())
+    posts.append(post.dict())
     return posts[-1]
-
 
 @app.get('/posts/{post_id}')
 def get_post(post_id: str):
-    for post in posts: #recorre por cada publicación la lista de publicaciones 
-        if post["id"] == post_id: #si el id de cada publicación es igual a cada parametro que me están pasando retorna esa publicación
+    for post in posts:
+        if post["id"] == post_id:
             return post
-    raise HTTPException(status_code=404, detail="Post no encontrado")
+    raise HTTPException(status_code=404, detail="Item not found")
 
-@app.delete("/posts/{post_id}")
+@app.delete('/posts/{post_id}')
 def delete_post(post_id: str):
     for index, post in enumerate(posts):
         if post["id"] == post_id:
             posts.pop(index)
-            return {"message": "la publicación ha sido eliminada"}
-    raise HTTPException(status_code=404, detail="Post no encontrado")
+            return {"message": "Post has been deleted succesfully"}
+    raise HTTPException(status_code=404, detail="Item not found")
 
 @app.put('/posts/{post_id}')
 def update_post(post_id: str, updatedPost: Post):
     for index, post in enumerate(posts):
         if post["id"] == post_id:
-            posts[index]["title"] = updatedPost.title
-            posts[index]["content"] = updatedPost.content
-            posts[index]["author"] = updatedPost.author
-            posts[index]["created_at"] = updatedPost.created_at
-            posts[index]["published_at"] = updatedPost.published_at
-            posts[index]["published"] = updatedPost.published
-            return {"message": "la publicación ha sido actualizada"}  
-    raise HTTPException(status_code=404, detail="Post no encontrado")
+            posts[index]["title"]= updatedPost.dict()["title"]
+            posts[index]["content"]= updatedPost.dict()["content"]
+            posts[index]["author"]= updatedPost.dict()["author"]
+            return {"message": "Post has been updated succesfully"}
+    raise HTTPException(status_code=404, detail="Item not found")
